29cf518f2700591721f771bd1c4ff3433e91029f
唐轶 <tangyi057@1caifu.com>
2018-05-17 12:34:38 +0800

    commit

--- src/components/Travel/Travel.vue
+++ src/components/Travel/Travel.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div style="width: 600px;min-height: 300px;margin: 60px auto 40px auto;">
-      <img src="https://jasonblog.oss-cn-hangzhou.aliyuncs.com/2%20%2889%29.jpg?Expires=1526534148&OSSAccessKeyId=TMP.AQFydQ2JKr7MwKR2TZkSCuUmxRfeP_ynXaQB_YV8e7R9AzhIctcYW-55bJ4gADAtAhQyLhThv0obW81RMO4T1i78tz5KgwIVALpTBQL4pzXiXiTGhXIr9BJb-PiP&Signature=7RdVHeBaqTZwtWke7jwbAVhWmRA%3D" class="content-img"/>
-      <img src="https://jasonblog.oss-cn-hangzhou.aliyuncs.com/2%20%2891%29.jpg?Expires=1526534223&OSSAccessKeyId=TMP.AQFydQ2JKr7MwKR2TZkSCuUmxRfeP_ynXaQB_YV8e7R9AzhIctcYW-55bJ4gADAtAhQyLhThv0obW81RMO4T1i78tz5KgwIVALpTBQL4pzXiXiTGhXIr9BJb-PiP&Signature=zs2DoXGOADBlf48FI2vNPtDsrzY%3D" class="content-img"/>
+      <img src="https://jasonblog.oss-cn-hangzhou.aliyuncs.com/2%20%2889%29.jpg?x-oss-process=style/shuiyin&Expires=1526535178&OSSAccessKeyId=TMP.AQFydQ2JKr7MwKR2TZkSCuUmxRfeP_ynXaQB_YV8e7R9AzhIctcYW-55bJ4gADAtAhQyLhThv0obW81RMO4T1i78tz5KgwIVALpTBQL4pzXiXiTGhXIr9BJb-PiP&Signature=dfWNmLuaXqg%2F%2F90ieyDYAzWQv1Q%3D" class="content-img"/>
+      <img src="https://jasonblog.oss-cn-hangzhou.aliyuncs.com/2%20%2891%29.jpg?x-oss-process=style/shuiyin&Expires=1526535229&OSSAccessKeyId=TMP.AQFydQ2JKr7MwKR2TZkSCuUmxRfeP_ynXaQB_YV8e7R9AzhIctcYW-55bJ4gADAtAhQyLhThv0obW81RMO4T1i78tz5KgwIVALpTBQL4pzXiXiTGhXIr9BJb-PiP&Signature=ngXMt5wizjpStxRsiNyMSD4vEdU%3D" class="content-img"/>
     </div>
   </div>
 </template>
